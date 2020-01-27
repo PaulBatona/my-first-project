1990a37c91ca6b8a2b2cbe4070b4117f0a6659ee
Adzhaev Oleg <o.adzhaev@gmail.com>
2020-01-27 20:49:27 +0300

    update app.js

--- src/App.js
+++ src/App.js
@@ -8,8 +8,11 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Edit <code>src/App.js</code> and save to reload. 000000
         </p>
+        <p>
+          Oleg
+          </p>
         <a
           className="App-link"
           href="https://reactjs.org"
